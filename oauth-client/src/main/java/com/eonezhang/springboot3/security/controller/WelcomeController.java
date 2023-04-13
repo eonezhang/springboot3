dd3b236cc06dd099c9a213989a7a43daedab42da
YunYong.Zhang <54343885@qq.com>
2023-04-13 21:37:24 +0800

    chore: adding home page view

--- oauth-client/src/main/java/com/eonezhang/springboot3/security/controller/WelcomeController.java
+++ oauth-client/src/main/java/com/eonezhang/springboot3/security/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package com.eonezhang.springboot3.security.controller;
 
 import com.eonezhang.springboot3.security.config.WelcomeClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +12,10 @@ public class WelcomeController {
     private final WelcomeClient welcomeClient;
 
 
-    public String welcome() {
-        String welcome = welcomeClient.getWelcome();
+    @GetMapping("/")
+    public String welcome(Authentication authentication) {
+//        String welcome = welcomeClient.getWelcome();
+        String welcome = authentication.getName();
         return "<h1>" + welcome + "</h1>";
     }
 }
